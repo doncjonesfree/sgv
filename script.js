770ec52f5e7c8edb1f284dc143383ddb94158644
Don Jones <doncjones1@gmail.com>
2018-12-18 14:16:12 -0800

    main path for evse project calc, text and email is working

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 // var request = new XMLHttpRequest();
-
-const api = 'http://localhost:3000/api/';
-const contractor_id = 'TcvgDDwhu96DiuxJC';  // SGV Electric's mongo id
-
-const request = new XMLHttpRequest();
 var ps = {};
 
+const runningLocal = true;
+
+const contractor_id = 'TcvgDDwhu96DiuxJC';  // SGV Electric's mongo id
 ps.phone = '(626) 629-8525';
+
+let baseUrl = '';
+if ( runningLocal ) {
+  baseUrl = 'http://localhost:3000/';
+} else {
+  baseUrl = 'https://www.electriciannearme.us/';
+}
+const api = baseUrl + 'api/';
+
+const request = new XMLHttpRequest();
+
+ps.project_price = function(id,projectType){
+  const url = baseUrl + 'api_price/' + projectType + '/' + contractor_id;
+  console.log('jones14',id,url);
+  $('#'+id).attr('src',url);
+};
 
 ps.setHtml = function(id,html) {
   document.getElementById(id).innerHTML = html;
@@ -19,13 +33,22 @@ ps.header1 = function(id) {
   let sz = 3;
   if ( width < 600 ) sz = 4;
 
-  console.log('jones14',width);
   let html = [];
   html.push('<div id="front_page_header">');
+
+  html.push('<a href="home.html">');
   html.push('<img id="h1_logo" src="img/SGV_Logo.jpg" style="height: 100px">');
-  html.push('<div class="h1_col1" style="font-size:' + sz + 'vw">Glendora</div>')
-  html.push('<div class="h1_col2" style="font-size:' + sz + 'vw">' + ps.phone + '</div>')
+  html.push('</a>');
+
+  html.push('<div class="h1_col1" style="font-size:' + sz + 'vw">Glendora');
   html.push('</div>');
+  html.push('<div class="h1_col2" style="font-size:' + sz + 'vw">');
+  html.push(ps.phone);
+  html.push('</div>');
+  html.push('<img id="glendora_logo" src="img/glendora-logo.png" style="height: 80px; margin: 10px 1em 10px 0;">');
+  html.push('</div>');
+
+
 
   ps.setHtml(id,html.join('\n'));
 };
@@ -36,17 +59,16 @@ ps.projectList = function(id){
     html.push('<div id="h_list">');
     for ( let i=0; i < list.length; i++ ) {
       const l = list[i];
-      html.push( sprintf('<div class="h_list_element" id="project_%s">%s</div>',l.value, l.label));
+      //html.push( sprintf('<div class="h_list_element" id="project_%s">%s</div>',l.value, l.label));
+      html.push( sprintf('<a class="h_list_element" href="%s.html">%s</a>',l.value, l.label));
     }
     html.push('</div>');
-    console.log('jones31',list);
     ps.setHtml(id,html.join('\n'));
   });
 
 };
 
 const getListOfProjectTypes = function(callback){
-  console.log('jones33a');
   const url = api + contractor_id + '/projectTypes';
   request.open('GET', url, true);
 
